2b54273164ba72b93363f2f16351d40fef15eebf
JGM <cnxw4570123@gmail.com>
2023-01-13 10:24:38 +0900

    day02 local

--- day01/day01_04_gui.py
+++ day01/day01_04_gui.py
@@ -1,4 +1,5 @@
-import tkinter as tk # mac에서도 지원하는 윈도우창 만드는 라이브러리
+import tkinter as tk
+# mac에서도 지원하는 윈도우창 만드는 라이브러리
 
 win = tk.Tk()
 win.geometry('400x300')
@@ -7,4 +8,3 @@ win.mainloop()
 # elixir
 # github와 같은 버전 컨트롤 시스템 왜 사용?
 # stage에 올린다
-
