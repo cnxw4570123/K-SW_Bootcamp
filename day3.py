99ca776bd85e4633b05c6e88c98674551b74fe9d
JGM <cnxw4570123@gmail.com>
2023-01-16 10:20:09 +0900

    day03_text01

---
+++ day3.py
@@ -0,0 +1,14 @@
+army = """우리는 국가와 국민에 충성을 다하는 대한민국 육군이다
+하나 우리는 자유민주주의를 수호하며 조국통일의 역군이 된다."""
+print(army)
+
+print('\t 안녕')
+
+start = 'Na ' * 4 + '\n'
+middle = 'hey ' * 3 + '\n'
+end = 'Goodbye .'
+print(start + start + middle + end)
+
+name = 'Henny'
+print(name.replace('H', 'P'))
+print('P'+name[1:])
