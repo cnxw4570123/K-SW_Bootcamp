985c478930f7b1f939c718786baaeb665efe0168
JGM <cnxw4570123@gmail.com>
2023-01-16 14:40:56 +0900

    day03_prime_number v0.6 break

--- day3.py
+++ day3.py
@@ -1,8 +1,7 @@
 # prime number
 
 number = int(input("input number :"))
-counts = 0
-
+is_prime = True
 # k = 1
 # while k < number:
 #     if number % k == 0:
@@ -11,9 +10,9 @@ counts = 0
 
 for i in range(2, number):
     if number % i == 0:
-        counts += 1
-
-if counts: # 비어있으면 False 따라서 0이 아니면 prime number 아님
-    print(number, 'is NOT a prime number')
-else:
+        is_prime = False
+        break
+if is_prime: # 비어있으면 False 따라서 0이 아니면 prime number 아님
     print(number, 'is a prime number')
+else:
+    print(number, 'is NOT a prime number')
