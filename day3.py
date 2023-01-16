14fcbe5c692aab1fb2a19459c9b5d3b5300adb32
JGM <cnxw4570123@gmail.com>
2023-01-16 14:34:53 +0900

    day03_prime_number v0.3

--- day3.py
+++ day3.py
@@ -9,11 +9,11 @@ counts = 0
 #         counts += 1
 #     k += 1
 
-for i in range(1, number+1):
+for i in range(2, number):
     if number % i == 0:
         counts += 1
 
-if counts == 2:
-    print(number, 'is a prime number')
-else:
+if counts: # 비어있으면 False 따라서 0이 아니면 prime number 아님
     print(number, 'is NOT a prime number')
+else:
+    print(number, 'is a prime number')
