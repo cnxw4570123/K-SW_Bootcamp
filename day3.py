80b8cbed446b3dba5452a3d01ddb8ac9740dd3ad
JGM <cnxw4570123@gmail.com>
2023-01-16 14:16:47 +0900

    day03_loop_for&while

--- day3.py
+++ day3.py
@@ -1,13 +1,19 @@
-# while
+# for
+
+word = 'thud'
+for letter in word:
+    if letter == 'u':
+        break
+    else:
+        print(letter)
+
 while True:
     dan = int(input('Dan(종료 원하면 \'0\' 입력): '))
     if dan == 0:
         break
     elif 1 < dan < 10:
-        i = 1
-        while i < 10:
-            print('{0} x {1} = {2}'.format(dan, i, dan * i))
-            i += 1
+        for i in range(1, 10):
+            print('{0} x {1} = {2}'.format(dan, i , dan*i))
     else:
         print('2~9 사이의 값을 입력 하세요.')
 print('종료')
