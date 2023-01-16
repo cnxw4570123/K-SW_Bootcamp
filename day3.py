7e6b79f740782f5058347ef8adaf20a454629214
JGM <cnxw4570123@gmail.com>
2023-01-16 15:35:45 +0900

    day03 prime number interval

--- day3.py
+++ day3.py
@@ -1,18 +1,18 @@
-# prime number
+# input 2 numbers
 
-number = int(input("input number :"))
-is_prime = True
-# k = 1
-# while k < number:
-#     if number % k == 0:
-#         counts += 1
-#     k += 1
+# print prime number between start and end
+start, end = input('give 2 numbers : ').split()
+if end < start:
+    start, end = end, start
 
-for i in range(2, number):
-    if number % i == 0:
-        is_prime = False
-        break
-if is_prime: # 비어있으면 False 따라서 0이 아니면 prime number 아님
-    print(number, 'is a prime number')
-else:
-    print(number, 'is NOT a prime number')
+for divide in range(int(start), int(end)):
+    # is_prime = True
+    if divide == 1:
+        continue
+    for k in range(2, divide):
+        if divide % k == 0:
+            # is_prime = False
+            break
+    # if is_prime:
+    else:
+        print(divide, end=" ")
