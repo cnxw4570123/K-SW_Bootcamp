b182c118dd7271a6dd0ed19b012c86bb55f1c7cc
JGM <cnxw4570123@gmail.com>
2023-01-16 14:00:05 +0900

    day03_mission_rev2

--- day03_misson.py
+++ day03_misson.py
@@ -6,5 +6,7 @@ That's - a moray!
 song_list = song.split()
 for i in range(len(song_list)):
     if song_list[i].startswith('m'):
-        song_list[i] = song_list[i].replace('m', 'M')
+        song_list[i] = song_list[i].title()
 print(' '.join(song_list))
+
+print('My kitty cat likes %s\n My kitty cat likes %s\n My Kitty cat fell on his %s And now thinks he\'s a %s ' % ('roast beef', 'ham', 'head', 'clam'))
