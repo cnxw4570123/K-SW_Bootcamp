8eda2f7c5d77f7be638bdf641685e91bd7191483
JGM <cnxw4570123@gmail.com>
2023-01-16 16:56:26 +0900

    Prob 6.2 + 6.3 random

--- day03_misson.py
+++ day03_misson.py
@@ -1,4 +1,14 @@
-list = ['Duck', 'Gourd', 'Spitz']
-print(f'{list[0]}y Mc{list[0]}face')
-print(f'{list[1]}y Mc{list[1]}face')
-print(f'{list[2]}y Mc{list[2]}face')
+# 6.2
+import random
+
+guess_me = random.randint(1, 10)
+
+while True:
+    number = int(input('give number :'))
+    if number < guess_me:
+        print("too low")
+    elif number == guess_me:
+        print('found it!')
+        break
+    else:
+        print('oops')
