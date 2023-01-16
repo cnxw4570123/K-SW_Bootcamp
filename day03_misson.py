163ee480a982e01f85cab100e45a6881ab4a4b1c
JGM <cnxw4570123@gmail.com>
2023-01-16 17:02:17 +0900

    Prob 6.2 + 6.3 attempts

--- day03_misson.py
+++ day03_misson.py
@@ -2,7 +2,7 @@
 import random
 
 guess_me = random.randint(1, 10)
-
+counts = 0;
 while True:
     number = int(input('give number :'))
     if number < guess_me:
@@ -12,3 +12,6 @@ while True:
         break
     else:
         print('oops')
+    counts += 1
+
+print(f'attempts = {counts}, guess Number = {guess_me}')
