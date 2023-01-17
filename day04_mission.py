936ef732d00a2cc1238e3ab008de5629020cf922
JGM <cnxw4570123@gmail.com>
2023-01-17 17:03:00 +0900

    Prob 8.4 ~ 8.5

--- day04_mission.py
+++ day04_mission.py
@@ -1,12 +1,15 @@
-# Prob 8.3
+# Prob 8.4 ~ 8.5
 e2f = {
     'dog': 'chien',
     'cat': 'chat',
     'walrus': 'morse'
 }
 
-f2e = dict()
-print(f2e)
-for eng_key, eng_val in e2f.items():
-    f2e[eng_val] = eng_key
-print(f2e)
+for eng_key in e2f:
+    if e2f[eng_key] == 'chien':
+        print(eng_key)
+
+print('-----------------------')
+
+for key in e2f.keys():
+    print(key)
