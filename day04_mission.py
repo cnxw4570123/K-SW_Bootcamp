eb4e8776ae7b080df208b4fdb4c6a42353c90367
JGM <cnxw4570123@gmail.com>
2023-01-17 16:50:13 +0900

    Prob 8.1 ~ 8.2

--- day04_mission.py
+++ day04_mission.py
@@ -1,18 +1,8 @@
-# Prob 7.11
-start1 = ['fee', 'fie', 'foe']
-rhymes = [
-    ('flop', 'get a mop'),
-    ('fope', 'turn the rope'),
-    ('fa', 'get your ma'),
-    ('fudge', 'call your ma'),
-    ('fat', 'pet the cat'),
-    ('fog', 'walk the dog'),
-    ('fun', 'say we\'re done'),
-]
-start2 = 'Someone better'
-
-for rhymeH, rhymeB in rhymes:
-    for st1_word in start1:
-        print(f'{st1_word.title()}!', end=' ')
-    print(f'{rhymeH.title()}!')  # unpacking
-    print(f'{start2} {rhymeB}')
+# Prob 8.1
+e2f = {
+    'dog': 'chien',
+    'cat': 'chat',
+    'walrus': 'morse'
+}
+print(e2f.get('walrus'))
+print(e2f['walrus'])
