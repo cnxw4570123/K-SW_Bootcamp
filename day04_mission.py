cd7298fe01250326b3c5a6e8feb756f24dd60aab
JGM <cnxw4570123@gmail.com>
2023-01-17 16:45:31 +0900

    Prob 7.11

--- day04_mission.py
+++ day04_mission.py
@@ -1,3 +1,18 @@
-# Prob 7.10
-even_list = [even_num for even_num in range(10) if even_num % 2 == 0]
-print(even_list)
+# Prob 7.11
+start1 = ['fee', 'fie', 'foe']
+rhymes = [
+    ('flop', 'get a mop'),
+    ('fope', 'turn the rope'),
+    ('fa', 'get your ma'),
+    ('fudge', 'call your ma'),
+    ('fat', 'pet the cat'),
+    ('fog', 'walk the dog'),
+    ('fun', 'say we\'re done'),
+]
+start2 = 'Someone better'
+
+for rhymeH, rhymeB in rhymes:
+    for st1_word in start1:
+        print(f'{st1_word.title()}!', end=' ')
+    print(f'{rhymeH.title()}!')  # unpacking
+    print(f'{start2} {rhymeB}')
