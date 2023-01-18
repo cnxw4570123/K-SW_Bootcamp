298b6782b21e7f85a54254816864edebf881cd0d
JGM <cnxw4570123@gmail.com>
2023-01-18 18:38:39 +0900

    Prob 9.2

--- day05_mission.py
+++ day05_mission.py
@@ -1,4 +1,18 @@
-# 9.1
-def good():
-    return ['Harry', 'Ron', 'Hermione']
+# 9.2
+def get_odds(first=0, end=10, step=0):
+    number = first
+    while number < end:
+        if number % 2 == 1:
+            yield number
+        number += 1
 
+
+count = 0
+for i in get_odds():
+    count += 1
+    if count == 3:
+        print(i)
+        break
+    
+
+    
