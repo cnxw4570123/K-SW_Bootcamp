67b4bb6ec843fb1b7dd8c2bb0e8df2bc5b36a1ab
JGM <cnxw4570123@gmail.com>
2023-01-18 17:05:25 +0900

    Prob 8.14

--- day05_mission.py
+++ day05_mission.py
@@ -1,13 +1,5 @@
-# 8.13
-# dict1 = dict()
-# for key, value in zip(('optimist', 'pessimist', 'troll'),('The glass is half full', 'The glass is half empty', 'How did you get a glass?')):
-#     dict1[key] = value
-
-# print(dict1)
-# dict1 = dict(zip(('optimist', 'pessimist', 'troll'),('The glass is half full', 'The glass is half empty', 'How did you get a glass?')))
-# print(dict1)
-
-keys = ('optimist', 'pessimist', 'troll')
-values = ('The glass is half full', 'The glass is half empty', 'How did you get a glass?')
-dict1 = dict(zip(keys, values))
-print(dict1)
+# 8.14
+titles = ['Creature of Habit', 'Crewel Fate']
+plots = ['A nun turns into a mon ster', 'A haunted yarn shop']
+movies = dict(zip(titles, plots))
+print(movies)
