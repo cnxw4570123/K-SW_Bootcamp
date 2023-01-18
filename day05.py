f4a2ff96e60da9275199da0270ea190d5360c669
JGM <cnxw4570123@gmail.com>
2023-01-18 13:34:42 +0900

    day05 function calc_fee v0.2

--- day05.py
+++ day05.py
@@ -1,41 +1,35 @@
 # function
-def calc_fee(*args):
+import random
+
+
+def calc_fee(ages) -> dict:
     """
     놀이공원 요금 계산 프로그램
-    :param args: ages
-    :return: total entrance fee
+    어른 몇 명? 아이 몇 명
+    :param ages: 나이
+    :return: {어른, }
     """
-    total = 0
-    for age in args:
+    fee_detail = {'adult': 0,
+                  'kids': 0,
+                  'total': {
+                      'counts': 0,
+                      'fee': 0}
+                  }
+    for age in ages:
         if age > 18:
-            total += 10_000
+            fee_detail['adult'] += 1
         else:
-            total += 3000
-    return total
+            fee_detail['kids'] += 1
+    fee_detail['total']['fee'] += 10_000 * fee_detail['adult'] + 3000 * fee_detail['kids']
+    fee_detail['total']['counts'] += fee_detail['adult'] + fee_detail['kids']
+    return fee_detail
 
 
-print(calc_fee(20, 20, 25))
-print(calc_fee(45, 43, 10, 7))
+n = int(input('몇 명이서 오셨나요? '))
 
-
-# def do_nothing():
-#     pass
-#
-#
-# def buggy(arg, result=[]):
-#     result.append(arg)
-#     print(result)
-#
-#
-# do_nothing()
-# print(do_nothing())
-#
-# mamamoo = ['화사', '솔라', '휘인', '문별']
-# print(mamamoo.pop())  # 리턴 후 삭제
-# print(mamamoo.remove('휘인'))  # 삭제만 -> 리턴 값 없으므로 None 출력
-#
-# buggy('a')
-# buggy('b')
-
-
-
+age_list = [random.randint(1, 50) for i in range(n)]
+print(age_list)
+receipt = calc_fee(age_list)
+print(receipt)
+print(f'총 {receipt["total"]["counts"]}분, 성인 : {receipt["adult"]}명'
+      f' 아이 : {receipt["kids"]}명\n총 금액 :{receipt["total"]["fee"]}원입니다.')
