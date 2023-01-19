ce664b29e94aeaa09ae3966e3871bc01365a2b5d
JGM <cnxw4570123@gmail.com>
2023-01-19 12:53:26 +0900

    day06 func recursive

--- day06.py
+++ day06.py
@@ -1,20 +1,32 @@
-# namespace
-g = 1  # global variable
+import time
+
+def factorial_iter(n):
+    """
+    반복문을 사용한 팩토리얼 함수
+    :param n: n!
+    :return: integer result
+    """
+    result = 1
+    for k in range(1, n+1):
+        result *= k
+    return result
 
 
-def print_global():
-    # g = 1 # local variable
-    print(g)
+def factorial_recu(n):
+    """
+    재귀함수를 사용한 팩토리얼 함수
+    :param n: n!
+    :return: 자기 자신을 호출 또는 1
+    """
+    if n == 1:
+        return 1
+    return n * factorial_recu(n-1)
 
 
-def change_print_global():
-    global g
-    print(g)
-    g = 2
-    print(g)
+bef_iter = time.process_time()
+print(factorial_iter(999))
+aft_iter = time.process_time()
+print(factorial_recu(999))
+aft_rec = time.process_time()
 
-
-change_print_global()
-print_global()
-print(globals())
-
+print(f'반복문 : {aft_iter - bef_iter}, 재귀문 : {aft_rec - aft_iter}')
