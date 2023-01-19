7657ae46d8ec37995c010403f60c11d06de2030a
JGM <cnxw4570123@gmail.com>
2023-01-19 11:22:10 +0900

    day06 func variables(global, local)

--- day06.py
+++ day06.py
@@ -1,24 +1,20 @@
-# decorator
-def document_info(f):
-    def new_func(*args, **kwargs):
-        print('실행중인 함수: ', f.__name__)
-        print('위치 기반 인수들 : ', args)
-        print('키워드 기반 인수들: ', kwargs)
-        result = f(*args, **kwargs)
-        print("실행 결과:", result)
-        return result
-    return new_func
+# namespace
+g = 1  # global variable
 
 
-@document_info  # decorator annotation 적용
-def sub_int(x, y):
-    return x - y
+def print_global():
+    # g = 1 # local variable
+    print(g)
 
 
-@document_info
-def squares(x, y):
-    return x**y
+def change_print_global():
+    global g
+    print(g)
+    g = 2
+    print(g)
 
 
-print(sub_int(7, 3), end="\n\n")
-print(squares(5, 2))
+change_print_global()
+print_global()
+print(globals())
+
