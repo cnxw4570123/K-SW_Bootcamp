eb8acfb3a37b02cd5b181bfb0aaa88fb22fa4567
JGM <cnxw4570123@gmail.com>
2023-01-19 15:48:22 +0900

    day06 class inheritance

--- day06.py
+++ day06.py
@@ -13,6 +13,12 @@ class Pokemon:
         print()
 
 
+class Pikachu(Pokemon):  # inheritance
+    pass
+
+
+pi1 = Pikachu('피카츄', '덴트', '몸통박치기')
+pi1.info()
 p1 = Pokemon('피카츄', '한지우', '50만 볼트/100만 볼트/번개')
 p2 = Pokemon('꼬부기', '오바람', '고속 스핀/거품/몸통 박치기/하이드로 펌프')
 for i in [p1, p2]:
