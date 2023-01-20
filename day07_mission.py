e4501bc56e92ee23849a09eba01bb1146be7bd3f
JGM <cnxw4570123@gmail.com>
2023-01-20 17:55:29 +0900

    Prob 10.9

--- day07_mission.py
+++ day07_mission.py
@@ -1,29 +1,24 @@
-# Prob 10.8
-class Element:
-    def __init__(self, name="", symbol="", number=0):
-        self.__name = name
-        self.__symbol = symbol
-        self.__number = number
+# 10.9
 
-    @property
-    def get_name(self):
-        return self.__name
-
-    @property
-    def get_symbol(self):
-        return self.__symbol
-
-    @property
-    def get_number(self):
-        return self.__number
-
-    def __str__(self):  # instance method 이므로 self 필요
-        return f'{self.get_name}, {self.get_symbol}, {self.get_number}'
+class Bear:
+    def eat(self):
+        return 'Berries'
 
 
-hydrogen = Element('Hydrogen', 'H', 1)
+class Rabbit:
+    def eat(self):
+        return 'Clover'
 
-print(hydrogen)
-hydrogen.__name = 'acid'
-# hydrogen._Element__name = 'acid' #이렇게 작성시에는 수정가능
-print(hydrogen)
+
+class Octothorpe:
+    def eat(self):
+        return 'Campers'
+
+
+b1 = Bear()
+r1 = Rabbit()
+o1 = Octothorpe()
+
+print(b1.eat())
+print(r1.eat())
+print(o1.eat())
