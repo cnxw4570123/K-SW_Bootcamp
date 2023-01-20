865a28df44ff7f59202a322600526a5bc70da716
JGM <cnxw4570123@gmail.com>
2023-01-20 14:40:59 +0900

    day07 class methods(instance, class, static)

--- day07.py
+++ day07.py
@@ -1,5 +1,9 @@
 class Duck:
+    color = 'red'
+    count = 0
+
     def __init__(self, input_name):
+        Duck.count += 1
         self.__hidden_name = input_name
 
     @property
@@ -13,13 +17,36 @@ class Duck:
         self.__hidden_name = input_name
     # name = property(get_name, set_name)
 
+    @classmethod
+    def kids(cls):
+        print('class method')
+        print(f"Duck has {cls.count} little kids")
+
+    @staticmethod
+    def test():
+        print('static method')
+
 
 don = Duck('Donald')
+print(don.color, Duck.color)
+don.color = 'blue'
+print(don.color, Duck.color)
+Duck.color = 'green'
+print(don.color, Duck.color)
+d2 = Duck('Induck')
+print(d2.color, don.color, Duck.color)
+Duck.kids()
+Duck.test()
+don.kids()
+don.test()
+
+# print(Duck.name)  # instance property이므로 class가 사용할 수 없다
 print(don.name)
 
-don.name = 'Donna'
-print(don.name)
 
-don._Duck_hidden_name = 'ww'
-don.name = 'Kim inha'
-print(don.name)
+# don.name = 'Donna'
+# print(don.name)
+#
+# don._Duck_hidden_name = 'ww'
+# don.name = 'Kim inha'
+# print(don.name)
