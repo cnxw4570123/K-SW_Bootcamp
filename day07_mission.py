86cc02511e6052c5aaea6e4fc97f8dbdfdac668e
JGM <cnxw4570123@gmail.com>
2023-01-20 17:11:13 +0900

    Prob 10.7

--- day07_mission.py
+++ day07_mission.py
@@ -1,14 +1,13 @@
-# Prob 10.6 
+# Prob 10.7
 class Element:
     def __init__(self, name="", symbol="", number=0):
         self.name = name
         self.symbol = symbol
         self.number = number
 
-    def dump(self):  # instance method 이므로 self 필요
-        print(self.name , self.symbol, self.number)
+    def __str__(self):  # instance method 이므로 self 필요
+        return f'{self.name}, {self.symbol}, {self.number}'
 
 
 hydrogen = Element('Hydrogen', 'H', 1)
-hydrogen.dump()
 print(hydrogen)
