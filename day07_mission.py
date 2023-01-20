2ed9220f4482003df8b59ee885246039d5b498da
JGM <cnxw4570123@gmail.com>
2023-01-20 16:57:27 +0900

    Prob 10.5

--- day07_mission.py
+++ day07_mission.py
@@ -1,14 +1,18 @@
 # Prob 10.4
 class Element:
-    def __init__(self, name, symbol, number):
+    def __init__(self, name="", symbol="", number=""):
         self.name = name
         self.symbol = symbol
         self.number = number
 
-    def get_item(self):
+    def get_item(self):  # instance method 이므로 self 필요
         return [self.name , self.symbol, self.number]
 
+el_dict = {
+    'name' : 'Hydrogen',
+    'symbol' : 'H',
+    'number' : 1
+}
 
-e1 = Element('Hydrogen', 'H', 1)
-print(e1.get_item())
-
+e2 = Element(**el_dict)
+print(e2.get_item())
