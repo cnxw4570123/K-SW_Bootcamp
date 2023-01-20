2f46ee987dee3d962466e31efc5dc8ed62ebb727
JGM <cnxw4570123@gmail.com>
2023-01-20 14:06:03 +0900

    day07 class @property

--- day07.py
+++ day07.py
@@ -1,21 +1,25 @@
-class PrettyMixin:
-    def dump(self):
-        import pprint
-        pprint.pprint(vars(self))
+class Duck:
+    def __init__(self, input_name):
+        self.__hidden_name = input_name
 
-    def time_print(self):
-        import datetime
-        print(datetime.date.today())
+    @property
+    def name(self):
+        print('inside the getter')
+        return self.__hidden_name
+
+    @name.setter
+    def name(self, input_name):
+        print('inside the setter')
+        self.__hidden_name = input_name
+    # name = property(get_name, set_name)
 
 
-class Thing(PrettyMixin):
-    pass
+don = Duck('Donald')
+print(don.name)
 
+don.name = 'Donna'
+print(don.name)
 
-t = Thing()
-t.age = 27
-t.name = 'gm'
-t.gender = 'male'
-t.dump()
-t.time_print()
-
+don._Duck_hidden_name = 'ww'
+don.name = 'Kim inha'
+print(don.name)
