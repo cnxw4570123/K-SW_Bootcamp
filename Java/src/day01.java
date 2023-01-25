665eea85d63155c437857f30359a2b83e97d06d6
JGM <cnxw4570123@gmail.com>
2023-01-25 15:43:25 +0900

    day08 dataType casting

--- Java/src/day01.java
+++ Java/src/day01.java
@@ -4,8 +4,23 @@ public class day01 {
       System.out.println("hello Java");
       System.out.println("Inha");
       String s = "University";
-      double q = 25.0412;
       int a = 10;
-      System.out.printf("%s\na = %d", s, a);
+//      float fv = 3.14f;
+      float fv = (float)3.14;
+      double q = 6.28;
+      System.out.printf("%s\na = %d\nq = %.2f\n", s, a, q);
+      System.out.println("fv = " + fv);
+      System.out.printf("fv = %07.2f\n", fv);
+      System.out.printf("fv = %7.2f\n", fv);
+      System.out.printf("fv = %-7.2f\n", fv);
+      System.out.printf("q = %.2f\n" , q);
+      char ch = 'A';
+      System.out.printf("%d\n", (int)ch);
+      String name = "JAVA";
+      System.out.println("name = " + name);
+
+      var number = 100;
+      final double PI = 3.14;
+
     }
 }
