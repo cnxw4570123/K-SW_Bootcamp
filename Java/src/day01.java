8ab01a8cef4825592ee5577bb489636b3c79658a
JGM <cnxw4570123@gmail.com>
2023-01-25 13:45:31 +0900

    day08 print methods

---
+++ Java/src/day01.java
@@ -0,0 +1,11 @@
+public class day01 {
+
+    public static void main(String[] args) {
+      System.out.println("hello Java");
+      System.out.println("Inha");
+      String s = "University";
+      double q = 25.0412;
+      int a = 10;
+      System.out.printf("%s\na = %d", s, a);
+    }
+}
