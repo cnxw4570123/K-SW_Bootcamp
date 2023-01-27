068d1e5aa4dc9687dcbb942860d1ca3c9547e9f2
JGM <cnxw4570123@gmail.com>
2023-01-27 15:53:55 +0900

    day10 Prob 02

--- Java/src/Probs/Ch03.java
+++ Java/src/Probs/Ch03.java
@@ -5,17 +5,12 @@ import java.util.Scanner;
 
 public class Ch03 {
     public static void main(String[] args) {
-
-        try{
-        if(new Scanner(System.in).nextInt() >= 19){
-            System.out.println("성년");
-        } else{
-            System.out.println("미성년");
-        }
-        } catch(InputMismatchException e){
-            System.out.println("숫자를 입력해주세요");
-        } catch (IndexOutOfBoundsException e1){
-            System.out.println("인덱스 문제");
+        System.out.print("등수를 입력해주세요 > ");
+        switch(new Scanner(System.in).nextInt()){
+            case 1 -> System.out.println("아주 잘했습니다");
+            case 2, 3 -> System.out.println("잘했습니다");
+            case 4, 5, 6 -> System.out.println("보통입니다");
+            default -> System.out.println("노력해야겠습니다");
         }
     }
 }
