883e4b984c3f6256e4b9c5d86a27d9b595a33b87
JGM <cnxw4570123@gmail.com>
2023-01-27 13:40:39 +0900

    day10 enhanced switch

--- Java/src/day03.java
+++ Java/src/day03.java
@@ -11,25 +11,15 @@ public class day03 {
     }
 
     public static void whoIsIt(String bio){
-        String kind = "";
-        switch (bio){
-            case "사자":
-            case "호랑이":
-                kind = "포유류";
-                break;
-            case "독수리":
-            case "참새":
-                kind = "조류";
-                break;
-            case "고등어":
-            case "참치":
-            case "새우":
-                kind = "어류";
-                break;
-            default:
+        String kind = switch (bio){
+            case "사자","호랑이" -> { yield "포유류"; }
+            case "독수리", "참새" -> { yield "조류"; }
+            case "고등어", "참치", "새우" -> { yield "어류"; }
+            default -> {
                 System.out.print("어이쿠! ");
-                kind = "...";
-        }
+                yield "...";
+            }
+        };
         System.out.printf("%s는 %s이다.\n" ,bio, kind);
     }
 }
