3dccdc7b079210e7807b5f0d2fabe9ae10c201b1
JGM <cnxw4570123@gmail.com>
2023-01-27 15:53:13 +0900

    day10 nested for with break continue

--- Java/src/day03.java
+++ Java/src/day03.java
@@ -1,7 +1,11 @@
 public class day03 {
     public static void main(String[] args) {
-        for(int i = 1; i < 10; i++){
+        for(int i = 2; i < 10; i++){
+//            if (i == 6)
+//                break;
             for(int j = 1; j < 10; j++){
+                if(j == 4)
+                    break;
                 System.out.println(i + " * " + j + " = " + i*j);
             }
             System.out.println("----------");
