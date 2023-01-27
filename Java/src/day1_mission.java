aa27b7382951cbbecd7b4aadec646577d1e498b9
JGM <cnxw4570123@gmail.com>
2023-01-27 09:37:24 +0900

    day08 Prob03

---
+++ Java/src/day1_mission.java
@@ -0,0 +1,13 @@
+public class day1_mission {
+    public static void main(String[] args) {
+        byte var1 = 127;
+        int var3 = 128;
+        var1 = (byte)var3;
+        System.out.println("var1 = " + var1);
+        float var5 = 123456.789123f;
+        System.out.println("var5 = " + var5);
+        double var6 = 123456.789123;
+        var5 = (float) var6;
+        System.out.println("var5 = " + var5);
+    }
+}
