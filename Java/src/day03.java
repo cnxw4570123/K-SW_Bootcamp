a74d4b972645283fd91e9aec85555d0d732f5702
JGM <cnxw4570123@gmail.com>
2023-01-27 10:47:37 +0900

    day10 method static

--- Java/src/day03.java
+++ Java/src/day03.java
@@ -2,19 +2,13 @@ import java.util.Scanner;
 
 public class day03 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Java 점수 입력(0 ~ 100) :");
-        int java_score = sc.nextInt();
-
-        if (java_score >= 90 && java_score <= 100){
-            if(java_score >= 95) System.out.println("최우수 학생");
-            System.out.println("A학점");
-        } else if(java_score >= 80) {
-            System.out.println("B학점");
-        } else if (java_score >= 70) {
-            System.out.println("C학점");
-        } else{
-            System.out.println("F학점");
-        }
+       int a = 3, b = 5;
+        System.out.printf("%d",sum(a, b));
     }
+    public static int sum(int i1, int i2){
+        int total = 0;
+        total = i1 + i2;
+        return total;
+    }
+
 }
