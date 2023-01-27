1388a418d4189dc1929bd89fffc0a98d2d55cd16
JGM <cnxw4570123@gmail.com>
2023-01-27 15:42:13 +0900

    day10 Prob 02

--- Java/src/Day03_mission.java
+++ Java/src/Day03_mission.java
@@ -1,17 +1,25 @@
+import java.util.Scanner;
+
 public class Day03_mission {
     public static void main(String[] args) {
-        for(int i = 1; i < 10; i++){
-            for(int j = 2; j < 6; j++){
-                System.out.print(j + " * " + i + " = " + j*i + "\t\t");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("소수구하기 프로그램");
+        System.out.print("소수를 구할 정수 한개 입력: ");
+        int number = sc.nextInt();
+        int prime_count = 0;
+        for(int i = 2; i <= number; i++){
+            int count = 0;
+            for(int j = 2; j < i; j++){ // i % j == 0 이면 소수 아님
+                if( i % j == 0) {
+                    count++;
+                    break;
+                }
             }
-            System.out.println();
-        }
-        System.out.println();
-        for(int i = 1; i < 10; i++){
-            for(int j = 6; j < 10; j++){
-                System.out.print(j + " * " + i + " = " + j*i + "\t\t");
+            if(count == 0){
+                prime_count++;
+                System.out.println(i + "은(는) 소수이다.");
             }
-            System.out.println();
         }
+        System.out.println("1~"+number+" 사이의 소수 개수 = " + prime_count);
     }
 }
