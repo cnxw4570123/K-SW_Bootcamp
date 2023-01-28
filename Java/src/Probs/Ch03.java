604f0dad89bff4b7a8b325e9f8349f2a36c565e5
JGM <cnxw4570123@gmail.com>
2023-01-29 02:05:05 +0900

    Ch03 Prob 03

--- Java/src/Probs/Ch03.java
+++ Java/src/Probs/Ch03.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 
 public class Ch03 {
     public static void main(String[] args) {
-        System.out.print("등수를 입력해주세요 > ");
-        switch(new Scanner(System.in).nextInt()){
-            case 1 -> System.out.println("아주 잘했습니다");
-            case 2, 3 -> System.out.println("잘했습니다");
-            case 4, 5, 6 -> System.out.println("보통입니다");
-            default -> System.out.println("노력해야겠습니다");
-        }
+        int sum = 0;
+        int input = 0;
+        do{
+            System.out.print("양의 정수를 입력하세요: ");
+            input = new Scanner(System.in).nextInt();
+            if(input % 2 == 0)
+                sum += input;
+        } while (input != -1);
+        System.out.println("입력한 양의 정수 중에서 짝수의 합은 " + sum);
     }
 }
