142c7dcc74408604335fd573fc1feb42735ab440
JGM <cnxw4570123@gmail.com>
2023-01-30 15:44:30 +0900

    day11 class getters & setters

--- Day10_Mission/src/Day03_Prob01.java
+++
@@ -1,2 +0,0 @@
-package PACKAGE_NAME;public class Day03_Prob01 {
-}
--- Java/src/Day11.java
+++ Java/src/Day11.java
@@ -1,11 +1,74 @@
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+        System.out.println("기본생성자");
+    }
+
+    public Person(String name, int age) {
+        System.out.println("매개변수 2개인 생성자");
+        this.name = name;
+        this.age = age;
+    }
+
+    public void talk() {
+        System.out.println("이름은 " + this.name + " 나이는 " + this.age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void breathe() {
+        System.out.println(this.name + "이 숨을 쉰다.");
+    }
+}
+
 public class Day11 {
-    public static void main(String[] args) {
-//        for(;;){ // 무한 반복문
-//            System.out.println("hi");
-//        }
-        int numList[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        for(int q : numList){
-            System.out.println("q = " + q);
-        }
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        Person ps = new Person();
+        ps.setName("길동");
+        ps.setAge(23);
+        ps.talk();
+        System.out.println(ps.getName());
+        ps.breathe();
+        System.out.println(ps);
+        Person hodong = new Person("호동", 45);
+        System.out.println(hodong);
+        System.out.print("이름 입력 >> ");
+        String name = sc.next();
+        System.out.print("나이 입력 >> ");
+        int age = sc.nextInt();
+        Person np = new Person(name, age);
+        System.out.println("np = " + np);
+
     }
 }
