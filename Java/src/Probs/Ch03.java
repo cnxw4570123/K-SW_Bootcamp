d37cb2fe264773e59ea055763f178b6d1732ebb0
JGM <cnxw4570123@gmail.com>
2023-01-30 11:22:57 +0900

    Ch03 Prob 08

--- Java/src/Probs/Ch03.java
+++ Java/src/Probs/Ch03.java
@@ -5,28 +5,18 @@ import java.util.Scanner;
 
 public class Ch03 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("철수 : ");
-        char cs = sc.next().charAt(0);
-        System.out.print("영희 : ");
-        char yh = sc.next().charAt(0);
-        if(cs == yh) {
-            System.out.println("무승부");
-        }else if(cs == 'p'){ // p r s
-            if(yh == 'r')
-                System.out.println("철수, 승!");
-            else
-                System.out.println("영희, 승!");
-        } else if (cs == 'r') {
-            if(yh == 's')
-                System.out.println("철수, 승!");
-            else
-                System.out.println("영희, 승!");
-        } else {
-            if(yh == 'p')
-                System.out.println("철수, 승!");
-            else
-                System.out.println("영희, 승!");
+        System.out.println(factorial(5));
+    }
+
+    static int factorial(int n){
+        switch (n) {
+            case 1 -> {
+                return 1;
+            }
+            default -> {
+                return n * factorial(n - 1);
+            }
         }
     }
+
 }
