9e190ad318e724c81c5e79cc8b53fe322b7856bf
JGM <cnxw4570123@gmail.com>
2023-01-30 10:11:21 +0900

    day11 foreach

---
+++ Java/src/Day11.java
@@ -0,0 +1,11 @@
+public class Day11 {
+    public static void main(String[] args) {
+//        for(;;){ // 무한 반복문
+//            System.out.println("hi");
+//        }
+        int numList[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for(int q : numList){
+            System.out.println("q = " + q);
+        }
+    }
+}
