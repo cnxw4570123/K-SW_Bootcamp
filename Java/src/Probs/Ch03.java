b357d79e9f938fbe9c704e53b497ac158c4c0af2
JGM <cnxw4570123@gmail.com>
2023-01-30 10:01:26 +0900

    Ch03 Prob 05

--- Java/src/Probs/Ch03.java
+++ Java/src/Probs/Ch03.java
@@ -1,12 +1,15 @@
 package Probs;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class Ch03 {
     public static void main(String[] args) {
-        for(int i = 1; i < 6; i++)
-            System.out.println("*".repeat(i));
-
+        double c;
+        for (double a = 1; a < 20; a++) {
+            for (double b = 1; b < 20; b++) {
+                c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+                if (c == (int)c && a + b + c <= 20) { 
+                    System.out.println("a = " + a + " b = " + b + " c = " + c);
+                }
+            }
+        }
     }
 }
