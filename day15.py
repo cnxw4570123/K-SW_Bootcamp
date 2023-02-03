2f1b6db4630fb0a1c86eecedfa14901a83731702
JGM <cnxw4570123@gmail.com>
2023-02-03 14:24:45 +0900

    Day15 linear list with 2list

--- day15.py
+++ day15.py
@@ -1,41 +1,38 @@
-def print_poly(px):
+def print_poly(px, tx):
     """
     다항식 출력
-    :param px:
+    :param px: 계수
+    :param tx: 차수
     :return:
     """
     term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
     polyStr = "P(x) = "
-
     for i in range(len(px)):
         coef = px[i]  # 계수
-        if i > 0 and coef > 0:
-            polyStr += "+"
-        elif coef == 0:
-            term -= 1
-            continue
+        term = tx[i]
 
+        if coef > 0:
+            polyStr += "+"
         if term > 0:
             polyStr += f"{coef}x^{term} "
         else:
             polyStr += f"{coef}"
-        term -= 1
 
     return polyStr
 
 
-def calc_poly(x_val, p_x):
+def calc_poly(x_val, px, tx):
     """
     다항식 계산
     :param x_val:
-    :param p_x:
+    :param px:
     :return:
     """
     ret_value = 0
-    term = len(p_x) - 1  # 최고차항 숫자 = 배열길이-1
+    term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
 
     for i in range(len(px)):
-        coef = p_x[i]  # 계수
+        coef = px[i]  # 계수
         ret_value += coef * x_val**term
         term -= 1
 
@@ -43,13 +40,14 @@ def calc_poly(x_val, p_x):
 
 
 # px = [7, -4, 0, 5]
-px = [-10, -2, 0, -1]
+px = [3, -4, 5]
+tx = [300, 20, 0]
 
 if __name__ == "__main__":
-    pStr = print_poly(px)
+    pStr = print_poly(px, tx)
     print(pStr)
 
     x_value = int(input("X 값-->"))
 
-    px_value = calc_poly(x_value, px)
+    px_value = calc_poly(x_value, px, tx)
     print(px_value)
