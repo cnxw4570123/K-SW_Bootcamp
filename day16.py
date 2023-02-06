8fd77e2b32078d0495410b9412805271fd8081f2
JGM <cnxw4570123@gmail.com>
2023-02-06 15:57:25 +0900

    day16 Stack 02

--- day16.py
+++ day16.py
@@ -1,36 +1,67 @@
-Stack = [None for _ in range(5)]
-top = -1
+def is_stack_full():
+    global SIZE, stack, top
+    if top >= SIZE - 1:
+        return True
+    return False
 
-top += 1
-Stack[top] = "커피"
-top += 1
-Stack[top] = "녹차"
-top += 1
-Stack[top] = "꿀물"
-top += 1
-Stack[top] = "바닐라라떼"
-top += 1
-Stack[top] = "아이스티"
-# top += 1  # stack overFlow
-# Stack[top] = "카라멜마끼아또"
 
-for i in range(len(Stack) - 1, -1, -1):
-    print(Stack[i])
+def is_stack_empty():
+    global SIZE, stack, top
+    if top == -1:
+        return True
+    return False
 
-data = Stack[top]
-Stack[top] = None
-top -= 1
-print(f"pop -> {data}")
 
-data = Stack[top]
-Stack[top] = None
-top -= 1
-print(f"pop -> {data}")
+def push(data):
+    global SIZE, stack, top
+    if is_stack_full():
+        print("stack is Full!")
+        return
+    top += 1
+    stack[top] = data
 
-data = Stack[top]
-Stack[top] = None
-top -= 1
-print(f"pop -> {data}")
 
-for i in range(len(Stack) - 1, -1, -1):
-    print(Stack[i])
+def pop():
+    global SIZE, stack, top
+    if is_stack_empty():
+        print("stack is Empty!")
+        return
+    data = stack[top]
+    stack[top] = None
+    top -= 1
+    return data
+
+
+def peek():
+    global SIZE, stack, top
+    if is_stack_empty():
+        print("stack is Empty!")
+        return None
+    return stack[top]
+
+
+SIZE = int(input("Stack Size : "))
+stack, top = [None for _ in range(SIZE)], -1
+
+if __name__ == "__main__":
+
+    while True:
+        menu = input("Insert(I)/Extract(E)/Verify(V)/Exit(X): ")
+        if menu == "X" or menu == "x":
+            break
+        if menu == "I" or menu == "i":
+            data = input("Input Data: ")
+            push(data)
+            print("Stack Status : ", stack)
+        elif menu == "E" or menu == "e":
+            data = pop()
+            print("Extracted Data ==> ", data)
+            print("Stack Status : ", stack)
+        elif menu == "V" or menu == "v":
+            data = peek()
+            print("Top is: ", data)
+            print("Stack Status: ", stack)
+        else:
+            print("입력이 잘못됨")
+
+    print("프로그램 종료!")
