888ccbf30722903603c114c6f36e6a73b59ddf7c
JGM <cnxw4570123@gmail.com>
2023-02-07 12:39:29 +0900

    LinkedList ex

--- Java/src/Day16/Day16.java
+++ Java/src/Day16/Day16.java
@@ -1,7 +1,16 @@
 package Day16;
 
+class LinkedList{
+    private Node head;
+    private int size;
+    public LinkedList(){
+    }
+}
+
 public class Day16 {
     public static void main(String[] args) {
+        LinkedList l_l = new LinkedList();
+
         Node n1 = new Node();
         n1.data = "피카츄";
         System.out.println(n1);
