80fb5a7086e87a8de2eaf157cc00899aae6b09aa
JGM <cnxw4570123@gmail.com>
2023-02-07 23:31:32 +0900

    day17 app03

--- day17_mission.py
+++ day17_mission.py
@@ -1,59 +1,65 @@
 import random
 
 
-class Queue:
-    time = 0
-
-    def __init__(self, size):
-        self.front = 0
-        self.rear = 0
-        self.size = size
-        self.items = [None for _ in range(size)]
-
-    def is_full(self):
-        return (self.rear + 1) % self.size == self.front
-
-    def is_empty(self):
-        return self.front == self.rear
-
-    def enqueue(self, data):
-        if self.is_full():
-            print("Queue is Full")
-            return
-        self.rear = (self.rear + 1) % self.size
-        self.items[self.rear] = data
-        Queue.time += data[1]
-
-    def dequeue(self):
-        if self.is_empty():
-            print("Queue is Empty!")
-            return
-        self.front = (self.front + 1) % self.size
-        data = self.items[self.front]
-        self.items[self.front] = None
-        Queue.time -= data[1]
+class Node:
+    def __init__(self, data=None):
+        self.left = None
+        self.data = data
+        self.right = None
 
     def __str__(self):
-        return f"Queue status : {self.items}"
+        return f"{self.data}"
+
+
+class BTree:
+    def __init__(self):
+        self.root = None
+
+    def append_child(self, x):
+        node = Node(x)
+        if not self.root:
+            self.root = node
+            return
+
+        current = self.root
+        while True:
+            if current.data > node.data:
+                if not current.left:
+                    current.left = node
+                    break
+                current = current.left
+
+            elif current.data < node.data:
+                if not current.right:
+                    current.right = node
+                    break
+                current = current.right
+            else:
+                return
 
     @staticmethod
-    def print_time():
-        print(f"Time to be waited : {Queue.time}")
+    def preorder(node):
+        if not node:
+            return None
+        print(node.data, end=" ")
+        BTree.preorder(node.left)
+        BTree.preorder(node.right)
 
 
 if __name__ == "__main__":
-    queue = Queue(5)
-    time_table = [("Inquery", 9), ("repair", 3), ("refund", 4), ("other", 1)]
+    data_arr = [
+        "cheetos",
+        "banana milk",
+        "powerade",
+        "shinramyeon",
+        "hershey's",
+        "sprite",
+        "pepsi",
+    ]
+    btree = BTree()
 
-    Queue.print_time()
-    print(queue)
+    for data in random.choices(data_arr, k=20):
+        btree.append_child(data)
 
-    for guest in random.choices(time_table, k=queue.size - 1):
-        queue.enqueue(guest)
-        Queue.print_time()
-        print(queue)
-    print("------------------------------------------------------------------")
-    for _ in range(queue.size - 1):
-        queue.dequeue()
-        Queue.print_time()
-        print(queue)
+    print("sold products with no duplicate -> ", end="")
+    BTree.preorder(btree.root)
