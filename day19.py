90dcd0baa5c308ce9dfef3bd8dacdfb824487e14
JGM <cnxw4570123@gmail.com>
2023-02-10 09:13:00 +0900

    day19 app01

--- day19.py
+++ day19.py
@@ -1,46 +1,27 @@
-import tkinter as tk
-
-memos = [None for _ in range(100)]  # 전역 리스트ㅡ
-memos[0], memos[1] = 0, 1
-
-
-def fact_recu(n):
-    if n == 1:
-        return 1
+def dec_to_bin(n):
+    if n < 2:
+        return n
     else:
-        return n * fact_recu(n - 1)
+        return f"{dec_to_bin(n // 2)}{n%2}"
 
 
-def fibo_memo_recu(n):
-    if n <= 1:
-        return memos[n]
-
-    if memos[n] is not None:  # 전역 메모리 memos에 이전에 계산한 결과 값이 존재하면
-        return memos[n]
-
-    memos[n] = fibo_memo_recu(n - 2) + fibo_memo_recu(n - 1)  # 처음 방문하는 n이면
-    return memos[n]
+def dec_to_oct(n):
+    if n < 8:
+        return n
+    else:
+        return f"{dec_to_oct(n // 8)}{n%8}"
 
 
-def fact_input():
-    lbl_result.config(text=f"팩토리얼 계산결과: {fact_recu(int(en_num_input.get()))}")
+def dec_to_hex(n):
+    rep_chr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    rep_chr += ["a", "b", "c", "d", "e", "f"]
+    if 10 <= n < 16:
+        return f"{rep_chr[n]}"
+    else:
+        return f"{dec_to_hex(n//16)}{rep_chr[n%16]}"
 
 
-def fibo_input():
-    lbl_result.config(text=f"피보나치 계산결과: {fibo_memo_recu(int(en_num_input.get()))}")
-
-
-win = tk.Tk()
-win.title("Calculator")
-win.geometry("300x150")
-
-en_num_input = tk.Entry()  # 텍스트 입력상자
-lbl_result = tk.Label(text="계산결과")
-btn_fact = tk.Button(text="팩토리얼", command=fact_input)
-btn_fibo = tk.Button(text="피보나치", command=fibo_input)
-
-en_num_input.pack()
-lbl_result.pack()
-btn_fact.pack(fill="x")
-btn_fibo.pack(fill="x")
-win.mainloop()
+print(dec_to_bin(65535))
+print()
+print(dec_to_oct(65535))
+print(dec_to_hex(65535))
