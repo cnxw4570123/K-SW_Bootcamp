d2f9e27fb6f7a8c558d6320aadd0fbeb7be3d945
JGM <cnxw4570123@gmail.com>
2023-02-14 00:50:28 +0900

    generic method 2

--- Java/src/Day21/Generic.java
+++ Java/src/Day21/Generic.java
@@ -3,8 +3,11 @@ package Day21;
 public class Generic{
     static class Prints{
         //generic method
-        public static <T> void PrintArray(T[] arr){
+        public static <T extends Number> void PrintArray(T[] arr){
             for(T a : arr) System.out.println(a);
+        }
+        public static <T> T getFirst(T[] arr){
+            return arr[0];
         }
     }
 
@@ -13,10 +16,11 @@ public class Generic{
         String s1[] = {"HI", "HELLO", "GREETING"};
         Integer i1[] = {10, 9, 7};
         Character c1[] = {'A', 'B', 'C'};
-        Prints.PrintArray(c1);
         Prints.PrintArray(d1);
         Prints.PrintArray(i1);
-        Prints.PrintArray(s1);
+        System.out.println(Prints.getFirst(c1));
+//        Prints.PrintArray(c1);
+//        Prints.PrintArray(s1);
 //        Prints.<Double>PrintArray(d1);
     }
 }
