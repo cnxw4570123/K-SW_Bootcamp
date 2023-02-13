5070eb4d95ed421f6236263e24186f0c11df8dc5
JGM <cnxw4570123@gmail.com>
2023-02-14 00:43:34 +0900

    generic method 1

--- Java/src/Day21/Generic.java
+++ Java/src/Day21/Generic.java
@@ -1,15 +1,22 @@
 package Day21;
 
 public class Generic{
+    static class Prints{
+        //generic method
+        public static <T> void PrintArray(T[] arr){
+            for(T a : arr) System.out.println(a);
+        }
+    }
+
     public static void main(String[] args) {
-//         Generic<String> postBox = new Generic<String>();
-        PostBox<String> postBox1 = new PostBox<String>(); // 현재는 생략 불가
-//        PostBox<int> postBox2 = new PostBox<Integer>(); // Primitive Type은 올 수 없고 Wrapper 클래스만 와야한다.
-        PostBox<Integer> postBox2 = new PostBox<Integer>();
-        postBox1.setItem("소포");
-//         postBox1.setItem(77);  // Type MisMatch
-         postBox2.setItem(77);
-        System.out.println(postBox1.getItem());
-        System.out.println(postBox2.getItem());
+        Double d1[] = {3.14, 2.71, 9.9};
+        String s1[] = {"HI", "HELLO", "GREETING"};
+        Integer i1[] = {10, 9, 7};
+        Character c1[] = {'A', 'B', 'C'};
+        Prints.PrintArray(c1);
+        Prints.PrintArray(d1);
+        Prints.PrintArray(i1);
+        Prints.PrintArray(s1);
+//        Prints.<Double>PrintArray(d1);
     }
 }
