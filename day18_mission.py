704ad8acd3ed6dc350faf611052385a22624c855
JGM <cnxw4570123@gmail.com>
2023-02-13 09:11:42 +0900

    23/02/13

--- day18_mission.py
+++ day18_mission.py
@@ -5,68 +5,88 @@ class Graph:
     def __init__(self, vertex, edges):
         self.vertex = vertex
         self.SIZE = len(vertex)
-        self.graph = [[0 for _ in range(self.SIZE)] for _ in range(self.SIZE)]
+        self.graph = [[False for _ in range(self.SIZE)] for _ in range(self.SIZE)]
         for edge in edges:
-            self.graph[vertex.index(edge[0])][vertex.index(edge[1])] = 1
-            self.graph[vertex.index(edge[1])][vertex.index(edge[0])] = 1
-
-    def snack_BFS(self):
-        # preparation
-        queue = deque([])
-        conv_list = []
-        # first vertex insertion
-        current = 0
-        queue.append(current)
-        conv_list.append(current)
-
-        next = None
-        for vtx in range(self.SIZE):
-            # if self.graph[current][vtx] == 1:
-            if self.graph[current][vtx]:
-                if vtx in conv_list:
-                    pass
-                else:
-                    next = vtx
-                    break
-
-        if current:  # if current is None
-            current = next
-            queue.append(current)
-            conv_list.append(current)
-        else:
-            current = queue.popleft()
-        return conv_list
-
-    def print_graph(self):
-        print("\t", end="")
-        for vtx in self.vertex:
-            print(f"{vtx:4s}")
-
-        for i in range(self.SIZE):
-            print(f"{self.vertex[i]:4s}", end="")
-            for j in range(self.SIZE):
-                print(f"{self.graph[i][j]:4d}", end="")
-        print("End")
+            self.graph[vertex.index(edge[0])][vertex.index(edge[1])] = True
+            self.graph[vertex.index(edge[1])][vertex.index(edge[0])] = True
 
 
-def make_graph():
-    global vertex, edges
-    vertex = input("Input vertexs with space: ").split()
-    vertex.sort()
+def make_graph():  # directed Graph without weight
+    global edges
+    # vertex = input("Input vertexs with space: ").split()
+    # vertex.sort()
+    vertex = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
 
-    # directed Graph without weight
-    edges = []
-    while True:
-        edge = input("Input edges with space (vertex vertex weight) (To quit > exit): ")
-        if edge == "exit" or edge == "EXIT":
-            break
-        edges.append(edge.split())
+    # while True:
+    #     edge = input("Input edges with space (vertex vertex weight) (To quit > exit): ")
+    #     if edge == "exit" or edge == "EXIT":
+    #         break
+    #     edges.append(edge.split())
+
+    # edges = []
+    edges = [
+        ["a", "b"],
+        ["a", "c"],
+        ["a", "e"],
+        ["b", "a"],
+        ["b", "c"],
+        ["b", "d"],
+        ["c", "a"],
+        ["c", "b"],
+        ["c", "d"],
+        ["c", "e"],
+        ["d", "b"],
+        ["d", "c"],
+        ["e", "a"],
+        ["e", "c"],
+        ["e", "h"],
+        ["e", "g"],
+        ["f", "c"],
+        ["g", "e"],
+        ["g", "i"],
+        ["h", "e"],
+        ["h", "i"],
+        ["i", "g"],
+        ["i", "h"],
+    ]
     g1 = Graph(vertex, edges)
     return g1
 
 
+def dfs_search(Graph, start, visited):
+    """
+    :param Graph: Graph instance
+    :param start: certain index of Graph
+    :param visited: record visted vertex
+    """
+
+    visited = [False for _ in range(Graph.SIZE)]
+    stack = [start]
+    visited[start] = True  # insert First Vertex
+
+    while stack:  # while  stack is not empty
+        v = stack.pop()
+        print(Graph.vertex[v], end=" -> ")
+
+        for i in range(Graph.SIZE):
+            if Graph.graph[v][i] and not visited[i]:  # if start adjacent
+                stack.append(i)
+                visited[i] = True
+    print("end")
+
+
+# def set_graph_value(g):
+#     values = []
+#     for i in range(g.SIZE):
+#         v_val = input("Input vertex with value( a 20 ) : ").split()
+#         values.append(v_val)
+#
+#     for vert_val in values:
+#         idx = g.vertex.index(vert_val[0])
+#         g.graph[idx][idx] = vert_val[1]
+
+
 edges = []
 g1 = make_graph()
-print(g1.snack_BFS())
-print("==")
-g1.print_graph()
+visited = []
+dfs_search(g1, 0, visited=visited)
