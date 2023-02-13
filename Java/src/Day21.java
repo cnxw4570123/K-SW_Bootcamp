9f01619cfa450e79a659a3d7a1d99e22b15cc01e
JGM <cnxw4570123@gmail.com>
2023-02-13 13:11:04 +0900

    Day21 rename Day20 -> Day21

--- Java/src/Day21.java
+++ Java/src/Day21.java
@@ -3,8 +3,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
 
-public class Day20 {
+public class Day21 {
     public static void main(String[] args) {
+
         try {
             URL url = new URL("https://www.inha.ac.kr");
             URLConnection urlConnection = url.openConnection();
