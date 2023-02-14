cb7749aaad860595be1e75d828fac0ec4b18deb3
JGM <cnxw4570123@gmail.com>
2023-02-14 10:18:07 +0900

    multicast

---
+++ Java/src/Day22/TimeClient.java
@@ -0,0 +1,31 @@
+package Day22;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class TimeClient {
+    public static void main(String[] args) {
+        System.out.println("타임 클라이언트 연결됨");
+        try (MulticastSocket socket = new MulticastSocket(10_000)) {
+            InetAddress group = InetAddress.getByName("224.0.0.117");
+            socket.joinGroup(group);
+            System.out.println("멀티 캐스트 타임 서버 그룹에 조인 완료");
+            byte[] buffer = new byte[256];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            for (int i = 0; i < 5; i++) {
+                socket.receive(packet);
+                String received = new String(packet.getData());
+                System.out.println(received.trim());
+            }
+            socket.leaveGroup(group); // 멀티 캐스트 타임 서버 탈출
+        } catch (IOException ex) {
+            System.out.println("입출력 오류");
+        }
+        System.out.println("타임 클라이언트 종료됨");
+    }
+
+
+}
+
