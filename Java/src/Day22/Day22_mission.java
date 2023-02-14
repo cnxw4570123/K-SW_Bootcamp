ca3d0e6f21b157df762b409c906ba7ece6ff852d
JGM <cnxw4570123@gmail.com>
2023-02-15 00:30:58 +0900

    Day22 Prob02

--- Java/src/Day22/Day22_mission.java
+++ Java/src/Day22/Day22_mission.java
@@ -1,24 +1,20 @@
 package Day22;
 
-class Worker extends Thread{
-    @Override
-    public void run() {
-        for(int i = 0; i < 5; i++){
-            System.out.printf("[%s] : %d\n", getName(), i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-}
-
 public class Day22_mission {
     public static void main(String[] args) {
         int alphabet = 'a';
         Thread main = Thread.currentThread();
-        Worker w = new Worker();
+        Thread w = new Thread(()-> {
+                for(int i = 0; i < 5; i++){
+                    System.out.printf("[%s] : %d\n", Thread.currentThread().getName(), i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        });
+        w.setName("Worker");
         w.start();
         for(int i = 0; i < 11; i++){
             System.out.println("["+main.getName()+ "]" + (char)(alphabet + i));
