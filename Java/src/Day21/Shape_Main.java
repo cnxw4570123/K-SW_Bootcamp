eaee0cc947eec4b16b8e69736b48646640a521be
JGM <cnxw4570123@gmail.com>
2023-02-14 09:16:30 +0900

    cutting unused Imports

--- Java/src/Day21/Shape_Main.java
+++ Java/src/Day21/Shape_Main.java
@@ -1,11 +1,8 @@
 package Day21;
 
-import java.util.List;
-
 public class Shape_Main {
     public static void main(String[] args) {
         System.out.println("사각형 : "+Shape.findShapes(Shape.shapes, shp -> shp.getType().equals("사각형")));
-    System.out.println("빨간 도형(면적 <= 12.0) : "+Shape.findShapes(Shape.shapes, shp -> shp.getColor().equals("빨간색") && shp.getArea() <= 12.0));
-
+        System.out.println("빨간 도형(면적 <= 12.0) : "+Shape.findShapes(Shape.shapes, shp -> shp.getColor().equals("빨간색") && shp.getArea() <= 12.0));
     }
 }
