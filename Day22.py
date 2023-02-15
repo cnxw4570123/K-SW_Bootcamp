5a0378e00c96d859ace9d686942984d9fc5aea84
JGM <cnxw4570123@gmail.com>
2023-02-15 14:38:12 +0900

    Day22 socket in Python

---
+++ Day22.py
@@ -0,0 +1,19 @@
+import socket
+
+
+if __name__ == "__main__":
+    print("에코 클라이언트 시작됨")
+    client_socket = socket.socket(
+        socket.AF_INET, socket.SOCK_STREAM  # TCP를 사용
+    )  # 어떤 방식으로 접근할 건지 조정
+    # client_socket.connect(("165.246.115.165", 20_000))
+    client_socket.connect(("127.0.0.1", 20_000))
+    print("서버에 연결됨")
+    while True:
+        msg = input("전송 메시지 입력 : ")
+        if msg == "exit" or msg == "EXIT":
+            break
+        client_socket.sendall((msg + "\n").encode())  # 송신
+        data = client_socket.recv(1024).decode()  # 수신
+        print(f"서버로 부터 받은 메시지 : {data}")
+    client_socket.close()
