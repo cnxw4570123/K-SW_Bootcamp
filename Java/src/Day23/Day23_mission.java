53d3d241f500fd18f79b3fe48a5993f60771e4ea
JGM <cnxw4570123@gmail.com>
2023-02-15 23:58:02 +0900

    Day23 Prob 02

--- Java/src/Day23/Day23_mission.java
+++ Java/src/Day23/Day23_mission.java
@@ -4,16 +4,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Day23_mission {
+
     public static void main(String[] args) {
-        String names[] = {"홍길동", "배장화", "임꺽정", "연흥부", "김선달", "황진이"};
-        List<String> nameList = Arrays.asList(names);
-        nameList.stream().filter(nm -> nm.charAt(0) < '이').forEach(s -> System.out.print(s + " "));
-        System.out.println();
-        nameList.stream().sorted().forEach(s -> System.out.print(s + " "));
-        System.out.println();
-        System.out.println(nameList.stream().findFirst());
-        nameList.stream().findFirst().ifPresent(System.out::println);
-        System.out.println(nameList.stream().count());
+        enum Gender{Man, Women}
+        List<String> names = List.of("홍길동", "배장화", "임꺽정", "연흥부", "김선달", "황진이");
+        List<Integer> ages = List.of(25, 20, 29, 28, 32, 18);
+        List<Gender> genders = List.of(Gender.Man, Gender.Women, Gender.Man, Gender.Man, Gender.Man, Gender.Women);
+
+        System.out.println(ages.stream().reduce(0,(a,b)-> a+b));
+        System.out.println(ages.stream().reduce(0, (a,b)-> a > b ? a:b));
+        System.out.println((double) ages.stream().reduce(0,Integer::sum)/ages.stream().count());
+
+
 
     }
 }
